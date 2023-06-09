5433438498a7cd0194766c03581750e7c0ea6e82
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-06-09 14:14:04 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var dbContext = new StudentDbContext())
+        {
+            // Create a new student
+            var student = new Student { Name = "John Doe" };
+
+            // Add the student to the DbSet and save changes
+            dbContext.Students.Add(student);
+            dbContext.SaveChanges();
+
+            Console.WriteLine("Student added successfully!");
+        }
+
+        Console.ReadLine();
+    }
+}
